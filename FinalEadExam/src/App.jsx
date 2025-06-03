192cb76e2e91433f55d00b4380750b1d7ba87bb9
KrishnaParhyar <parhyarkrishna@gmail.com>
2025-06-03 12:04:02 +0500

    initial commit

---
+++ FinalEadExam/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TrafficLightSimulator from './components/TrafficLightSimulator'
+
+function App() {
+  
+
+  return (
+    <>
+      <TrafficLightSimulator />
+    </>
+  )
+}
+
+export default App
